59088c0a2b0826d43f5681f91c3ccf21b729470b
龙腾道 <LongTengDao@LongTengDao.com>
2021-02-23 23:48:01 +0800

    bugfix: www.then.com

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const www = new Proxy(new URL('https://www'), {
+const www = new Proxy(() => 'https://www', {
     get: function get(target, prop) {
-        let o = Reflect.get(target, prop);
-        if (typeof o === 'function') {
-            return o.bind(target)
-        }
-        if (typeof prop !== 'string') {
-            return o;
-        }
-        if (prop === 'then') {
-            return Promise.prototype.then.bind(fetch(target));
-        }
-        target = new URL(target);
-        target.hostname += `.${prop}`;
-        return new Proxy(target, { get });
+        if (typeof prop !== 'string') { return o; }
+        const hostname = `${target()}.${prop}`;
+        return new Proxy(
+            () => hostname,
+            prop === 'then'
+                ? { apply: (target, thisArg, args) => fetch(target()).then(...args),
+                    get }
+                : { get }
+        );
     }
 });
