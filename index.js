e53c5119d9539d49a83fea1862672555943b8555
龙腾道 <LongTengDao@LongTengDao.com>
2021-02-24 01:16:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const www = function () {
-    const handlers = {
-        apply: (target, thisArg, args) =>
-            fetch(target().slice(0, -5)).then(...args),
-        get: (target, key) =>
-            new Proxy(() => `${target()}.${key}`, handlers),
-    };
-    return new Proxy(() => 'https://www', handlers);
-}();
+const www = new Proxy(() => 'https://www', {
+    get (target, key) { return new Proxy(() => `${target()}.${key}`, this); },
+    apply: (target, thisArg, args) => fetch(target().slice(0, -5)).then(...args),
+});
