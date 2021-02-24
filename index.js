162c2856d7c15cf2646530a2566724c58909e383
龙腾道 <LongTengDao@LongTengDao.com>
2021-02-24 14:00:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const www = new Proxy(() => 'https://www', {
-    get (target, key) { return new Proxy(() => `${target()}.${key}`, this); },
-    apply: (target, thisArg, args) => fetch(target().slice(0, -5)).then(...args),
+    get(target, key) {
+        if (typeof key === 'string') {
+            return new Proxy(() => target() + '.' + key, this);
+        }
+        if (key === Symbol.toPrimitive) {
+            return () => target() + '/';
+        }
+    },
+    apply(target, thisArg, args) {
+        switch (typeof args[0]) {
+            case 'function':
+                return fetch(target().slice(0, -5)).then(...args);
+            case 'object':
+                const rest = String.raw(...args);
+                return { then: () => fetch(target() + '/' + rest).then(...arguments) };
+            case 'string':
+                return { then: () => fetch(target() + '/' + arg[0]).then(...arguments) };
+        }
+    },
 });
